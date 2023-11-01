7519b86819d534a68440b51581a5b4de0cba1a4e
Ashwini Mahajan <ashwini.mh@outlook.com>
2023-11-01 08:39:43 +0530

    Java Maven Assignment

---
+++ employee/employee/src/main/java/com/employee/employee/EmpSystemApplication.java
@@ -0,0 +1,13 @@
+package com.employee.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpSystemApplication.class, args);
+	}
+
+}
